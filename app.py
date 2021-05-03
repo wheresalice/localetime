b9d1aff9eb7a1db4f9010c2fadfbb16d05fd87b0
WheresAlice <WheresAlice@users.noreply.github.com>
2021-05-03 17:38:57 +0100

    add date

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from babel.dates import format_time
+from babel.dates import format_time, format_date
 
 from flask import Flask, request
 
@@ -10,8 +10,9 @@ app = Flask(__name__)
 def time():
     locale = request.args.get("locale", 'en_GB')
     date_time = datetime.now()
-    locale_time = format_time(date_time, locale=locale)
-    return f'<h1 style="font-size:13em;text-align:center">{locale_time}</h1>'
+    locale_time = format_time(date_time, format='short', locale=locale)
+    locale_date = format_date(date_time, locale=locale)
+    return f'<div><h1 style="font-size:13em;text-align:center">{locale_time}</h1></div><div><h1 style="font-size:13em;text-align:center">{locale_date}</h1></div>'
 
 
 if __name__ == '__main__':
